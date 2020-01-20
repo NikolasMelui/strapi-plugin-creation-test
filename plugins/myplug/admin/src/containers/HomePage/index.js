e7e1b5f8827697d57446e2e0cea4df8bb5434117
nikolasmelui <slak@samaradom.ru>
2020-01-20 17:59:21 +0400

    Add some simple fetch logic to the HomePage of the plugin

--- plugins/myplug/admin/src/containers/HomePage/index.js
+++ plugins/myplug/admin/src/containers/HomePage/index.js
@@ -10,11 +10,23 @@ import React, { memo } from "react";
 import pluginId from "../../pluginId";
 
 class HomePage extends React.Component {
+  state = {
+    information: {
+      data: "There is no data yet"
+    }
+  };
+
+  async componentDidMount() {
+    const response = await fetch("http://localhost:1337/banners");
+    const information = await response.json();
+    this.setState({ information: JSON.stringify(information) });
+  }
+
   render() {
     return (
       <div>
         <h1>{pluginId}&apos;s HomePage</h1>
-        <p>First component build with plugins</p>
+        <p>{this.state.information}</p>
       </div>
     );
   }
